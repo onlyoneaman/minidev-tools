0d6ec8e90a69df52bb23e1bdd089844fb7a1231f
onlyoneaman <2000.aman.sinha@gmail.com>
2024-03-02 05:53:20 +0530

    header link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const Header = () => {
 
   return (
     <header
       className="container"
     >
-      <h1>
+      <Link href="/">
         Mini Tools
-      </h1>
+      </Link>
     </header>
   )
 };
