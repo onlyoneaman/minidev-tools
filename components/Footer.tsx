2f604aed0996acd46f82d42b2410481b203c686d
onlyoneaman <2000.aman.sinha@gmail.com>
2024-03-02 05:52:02 +0530

    Fix Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,14 @@
 const Footer = () => {
 
   return (
-    <footer>
+    <footer
+      className="border-t  text-center text-sm p-3"
+    >
       <p>© 2024</p>
+      <span>
+        Created by <a className="underline" target="_blank" href="https://twitter.com/onlyoneaman">Aman</a>
+        &nbsp;|&nbsp;<a target="_blank" href="https://github.com/onlyoneaman/mini-tools">Github</a>
+      </span>
     </footer>
   )
 };
