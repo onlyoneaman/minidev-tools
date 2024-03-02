4b0f0ad91d770451515f4849005ca20b3b038880
onlyoneaman <2000.aman.sinha@gmail.com>
2024-03-02 05:42:14 +0530

    Fix Build Issue

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,15 +2,19 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import React from "react";
 
-const Layout: React.FC = ({children}) => {
+type LayoutProps = {
+  children: React.ReactNode;
+}
+
+const Layout = ({children}: LayoutProps) => {
 
   return (
     <div className={"bg-white text-black p-3"}>
-      <Header />
+      <Header/>
       <main className="container mx-auto">
         {children}
       </main>
-      <Footer />
+      <Footer/>
     </div>
   );
 };
