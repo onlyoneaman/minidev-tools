09b9ab55746d51a66d1caf5833091cb2de20bd30
onlyoneaman <2000.aman.sinha@gmail.com>
2024-03-04 00:34:24 +0530

    Clarity

---
+++ components/HeadContent.tsx
@@ -0,0 +1,43 @@
+// HeadContent.tsx
+import React from 'react';
+import Head from 'next/head';
+
+const HeadContent = () => {
+
+  const clarityId = process.env.NEXT_PUBLIC_CLARITY_ID;
+  const clarityScript = clarityId ? `
+  <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${clarityId});
+</script>`: ''
+
+  return (
+    <Head>
+      <title>Home - minidev.tools</title>
+      <meta
+        name="description"
+        content="Free tools for developers like encode/decode, json formatter, minifiers, etc. No ads, no tracking, no paywalls."
+        key="desc"
+      />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <meta
+        name="keywords"
+        content="decode, text, html, generate, formatbeautify, formatter, generator, code, freetools"
+      />
+
+      <link rel="icon" href="/public/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+
+      {clarityScript && <script dangerouslySetInnerHTML={{ __html: clarityScript }} />}
+    </Head>
+  );
+};
+
+export default HeadContent;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,15 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import React from 'react';
-import Head from 'next/head';
 import Layout from "@/components/Layouts/Layout";
 import {ThemeProvider} from "@/components/ThemeProvider";
+import HeadContent from "@/components/HeadContent";
 
 const MyApp = ({Component, pageProps}: AppProps) => {
 
   return (
     <>
-      <Head>
-        <title>
-          Home - minidev.tools
-        </title>
-        <meta
-          name="description"
-          content="Free tools for developers like encode/decode, json formatter, minifiers, etc. No ads, no tracking, no paywalls."
-          key="desc"
-        />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-
-        <meta
-          name="keywords"
-          content="decode, text, html, generate, formatbeautify, formatter, generator, code, freetools"
-        />
-
-        <link rel="icon" href="/public/favicon.ico"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-
-      </Head>
+      <HeadContent />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
