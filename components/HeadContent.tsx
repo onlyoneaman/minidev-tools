5d57ab7c4a477d28ec3d5590a68eb3311920c085
onlyoneaman <2000.aman.sinha@gmail.com>
2024-03-10 18:09:24 +0530

    Script using next/script

--- components/HeadContent.tsx
+++ components/HeadContent.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 const HeadContent = () => {
 
@@ -18,48 +19,58 @@ const HeadContent = () => {
   const fullTitle = `${title} | minidev.tools`;
 
   return (
-    <Head>
-      <title>
-        {fullTitle}
-      </title>
-      <meta
-        name="description"
-        content={description}
-        key="desc"
-      />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-      <meta
-        name="keywords"
-        content="decode, text, html, generate, formatbeautify, formatter, generator, code, freetools"
-      />
-      <link rel="icon" href="/public/favicon.ico"/>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-      <link rel="manifest" href="/site.webmanifest"/>
+    <>
+      <Head>
+        <title>
+          {fullTitle}
+        </title>
+        <meta
+          name="description"
+          content={description}
+          key="desc"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <meta
+          name="keywords"
+          content="decode, text, html, generate, formatbeautify, formatter, generator, code, freetools"
+        />
+        <link rel="icon" href="/public/favicon.ico"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
 
-      <meta property="og:title" content="minidev.tools"/>
-      <meta property="og:description" content={description}/>
-      <meta property="og:url" content="https://minidev.tools"/>
-      <meta property="og:type" content="website"/>
-      <meta property="og:site_name" content="minidev.tools"/>
-      <meta property="og:image" content={ogImage}/>
-      <meta property="og:image:width" content="1270"/>
-      <meta property="og:image:height" content="812"/>
-      <meta property="og:image:alt" content="minidev.tools"/>
-      <meta property="og:locale" content="en_US"/>
+        <meta property="og:title" content="minidev.tools"/>
+        <meta property="og:description" content={description}/>
+        <meta property="og:url" content="https://minidev.tools"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:site_name" content="minidev.tools"/>
+        <meta property="og:image" content={ogImage}/>
+        <meta property="og:image:width" content="1270"/>
+        <meta property="og:image:height" content="812"/>
+        <meta property="og:image:alt" content="minidev.tools"/>
+        <meta property="og:locale" content="en_US"/>
 
-      <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:creator" content="@onlyoneaman"/>
-      <meta property="twitter:title" content="minidev.tools"/>
-      <meta property="twitter:description" content={description}/>
-      <meta property="twitter:image" content={ogImage}/>
-      <meta property="twitter:image:alt" content="minidev.tools"/>
-      <meta property="twitter:url" content="https://minidev.tools"/>
-      <meta property="twitter:domain" content="minidev.tools"/>
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:creator" content="@onlyoneaman"/>
+        <meta property="twitter:title" content="minidev.tools"/>
+        <meta property="twitter:description" content={description}/>
+        <meta property="twitter:image" content={ogImage}/>
+        <meta property="twitter:image:alt" content="minidev.tools"/>
+        <meta property="twitter:url" content="https://minidev.tools"/>
+        <meta property="twitter:domain" content="minidev.tools"/>
 
-      {clarityId && <script dangerouslySetInnerHTML={{__html: clarityScript}}/>}
-    </Head>
+      </Head>
+      {
+        clarityId && (
+          <Script
+            strategy="afterInteractive"
+            id="clarity-script"
+            dangerouslySetInnerHTML={{__html: clarityScript}}
+          />
+        )
+      }
+    </>
   );
 };
 
